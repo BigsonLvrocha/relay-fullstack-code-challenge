cd5c2b8f37a627ba43ded1a277137d8ac30e673a
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-02-21 23:48:38 -0300

    style(eslint): add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'plugin:prettier/recommended'
   ],
   globals: {
     Atomics: 'readonly',
@@ -23,7 +24,6 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'prettier',
   ],
-  rules: {
-  },
 };
