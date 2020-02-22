c37f13ccdb165fb2641d1dc0a413ffcf3f82d3de
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-02-22 00:47:51 -0300

    feat(router): add graphql to router

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,3 +1,7 @@
+import { execute } from 'graphql-api-koa';
+
+import { schema } from './graphql';
+
 import Router = require('@koa/router');
 
 const router = new Router();
@@ -5,5 +9,12 @@ const router = new Router();
 router.get('/hello', ctx => {
   ctx.body = 'hello visitor';
 });
+
+router.post(
+  '/graphql',
+  execute({
+    schema,
+  }),
+);
 
 export { router };
