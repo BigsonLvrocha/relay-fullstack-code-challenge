3a911876c56bc989b8e2b57e45eba547e71e06fa
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-02-28 01:07:21 -0300

    feat(users): add model

---
+++ packages/api/src/db/models/User.ts
@@ -0,0 +1,47 @@
+import { Model, Sequelize, DataTypes, BuildOptions } from 'sequelize';
+
+interface User extends Model {
+  readonly id: string;
+  name: string;
+  email: string;
+  password_hash: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+type UserStatic = typeof Model & {
+  new (values?: Partial<User>, options?: BuildOptions): User;
+};
+
+export function build(sequelize: Sequelize) {
+  const User = sequelize.define(
+    'user',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: 'create_at',
+      updatedAt: 'updated_at',
+    },
+  ) as UserStatic;
+  return User;
+}
