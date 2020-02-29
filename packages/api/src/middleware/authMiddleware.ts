f6ba0ddd003e3caa6426c97b1a23e775e1ae6d1f
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-02-29 09:26:14 -0300

    feat(auth-middleware): add auth middleware

---
+++ packages/api/src/middleware/authMiddleware.ts
@@ -0,0 +1,36 @@
+import { promisify } from 'util';
+
+import { verify, GetPublicKeyOrSecret, Secret } from 'jsonwebtoken';
+import { Middleware } from 'koa';
+
+import { models } from '../db';
+
+const verifyAsync = promisify<string, Secret | GetPublicKeyOrSecret, object>(
+  verify,
+);
+
+export const authMiddleware: Middleware = async (ctx, next) => {
+  const auth =
+    ctx.request.headers.authorization || ctx.request.headers.Authorization;
+  if (!auth) {
+    return next();
+  }
+  const [bearer, token] = auth.split(' ');
+  if (bearer !== 'Bearer') {
+    throw new Error('invalid token');
+  }
+  const tokenData = (await verifyAsync(
+    token,
+    process.env.API_SECRET as string,
+  )) as { userId?: string };
+  const { userId } = tokenData;
+  if (!userId) {
+    throw new Error('invalid token');
+  }
+  const user = await models.User.findByPk(userId);
+  if (!user) {
+    throw new Error('invalid token');
+  }
+  ctx.state.user = user;
+  return next();
+};
