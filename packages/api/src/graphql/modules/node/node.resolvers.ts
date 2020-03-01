efe104bcbab0ae548c0a0cf910a7c1b591b91673
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-03-01 13:11:08 -0300

    feat(node): add node query

---
+++ packages/api/src/graphql/modules/node/node.resolvers.ts
@@ -0,0 +1,35 @@
+import { fromGlobalId } from 'graphql-relay';
+
+import { GQLResolvers } from '../../generated/schema';
+import { GraphQLContext } from '../../context';
+import { Models } from '../../../db';
+
+function isSupportedType(
+  type: string,
+  ctx: GraphQLContext,
+): type is keyof Models {
+  return type in ctx.models;
+}
+
+const resolvers: GQLResolvers = {
+  Query: {
+    async node(_parent, args, ctx) {
+      const { type, id } = fromGlobalId(args.id);
+      if (!isSupportedType(type, ctx)) {
+        return null;
+      }
+      const node = await ctx.models[type].findByPk(id, {
+        raw: true,
+      });
+      if (!node) {
+        return null;
+      }
+      return {
+        ...node,
+        __typename: type,
+      };
+    },
+  },
+};
+
+export default resolvers;
