b2b0ce7208f2998eac748e2ae9ed703dca4ef0f3
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-03-08 08:42:36 -0300

    feat(recipient): add recipient to models

--- packages/api/src/db/index.ts
+++ packages/api/src/db/index.ts
@@ -1,7 +1,8 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Model, BuildOptions } from 'sequelize';
 
 import * as config from './config';
 import * as User from './models/User';
+import * as Recipient from './models/Recipient';
 
 function isNodeEnvValid(env?: string): env is keyof typeof config {
   return !!env && env in config;
@@ -20,9 +21,30 @@ export const sequelize = new Sequelize(seqConfig);
 function buildModel(seq: Sequelize) {
   return {
     User: User.build(seq),
+    Recipient: Recipient.build(seq),
   };
 }
 
 export const models = buildModel(sequelize);
 
 export type Models = ReturnType<typeof buildModel>;
+
+type AvailableModelKeys = keyof Models;
+
+type AvailableModels = Models[AvailableModelKeys];
+
+type SequelizeInstanceType<TStatic> = TStatic extends typeof Model & {
+  new (values?: Partial<infer U>, options?: BuildOptions): infer U;
+}
+  ? U
+  : never;
+
+export type AvailableModelInstanceTypes = SequelizeInstanceType<
+  AvailableModels
+>;
+
+export type SequelizeStaticType<TInstance> = typeof Model & {
+  new (values?: Partial<TInstance>, options?: BuildOptions): TInstance;
+};
+
+export type AnyModel = SequelizeStaticType<AvailableModelInstanceTypes>;
