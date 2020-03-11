dfd85b91177911ff99bdc9d69df89728ccf09525
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-03-10 23:53:58 -0300

    feat(upload): add upload mutations

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,9 +1,12 @@
-import { execute } from 'graphql-api-koa';
+import { resolve } from 'path';
 
+import { execute } from 'graphql-api-koa';
+import { graphqlUploadKoa } from 'graphql-upload';
 import koaPlayground from 'graphql-playground-middleware-koa';
 import { schema } from './graphql';
 import { getContext } from './graphql/context';
 
+import send = require('koa-send');
 import Router = require('@koa/router');
 
 const router = new Router();
@@ -14,8 +17,18 @@ router.get('/hello', ctx => {
 
 router.all('/playground', koaPlayground({ endpoint: '/graphql' }));
 
+router.get('/uploads/:file', ctx => {
+  return send(ctx, ctx.params.file, {
+    root: resolve(__dirname, '..', 'tmp', 'uploads'),
+  });
+});
+
 router.post(
   '/graphql',
+  graphqlUploadKoa({
+    maxFiles: 10,
+    maxFileSize: 10000000,
+  }),
   execute({
     override: ctx => {
       return {
