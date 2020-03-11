d4d56d770868fe703d8fde4a0db618e37d4202d2
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-03-10 23:53:33 -0300

    fix(data-loader): add missing types loader

--- packages/api/src/graphql/dataloaders/index.ts
+++ packages/api/src/graphql/dataloaders/index.ts
@@ -1,10 +1,14 @@
 import { Models } from '../../db';
 import { getLoader as getRecipientLoader } from './Recipient';
 import { getLoader as getUserLoader } from './User';
+import { getLoader as getAvatarLoader } from './Avatar';
+import { getLoader as getDeliveryManLoader } from './DeliveryMan';
 
 export function getLoaders(models: Models) {
   return {
     User: getUserLoader(models),
     Recipient: getRecipientLoader(models),
+    Avatar: getAvatarLoader(models),
+    DeliveryMan: getDeliveryManLoader(models),
   };
 }
