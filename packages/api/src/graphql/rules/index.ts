fb80399b41a0daa4f159faae09680f3f1cc55f17
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-03-12 23:32:38 -0300

    fix(shield): add custom rule for error

--- packages/api/src/graphql/rules/index.ts
+++ packages/api/src/graphql/rules/index.ts
@@ -2,8 +2,13 @@ import { rule } from 'graphql-shield';
 
 import { GraphQLContext } from '../context';
 
+import createError = require('http-errors');
+
 export const isAdmin = rule({ cache: 'contextual' })(
   (_parent, _arg, ctx: GraphQLContext) => {
-    return !!ctx.user;
+    if (!ctx.user) {
+      return createError(401, 'Not authorized');
+    }
+    return true;
   },
 );
