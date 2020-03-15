05d1b27552eca62b4aa6834e766491a8a9417aaf
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-03-15 12:09:17 -0300

    feat(delivery): add delivery resolver and create delivery mutation

---
+++ packages/api/src/graphql/modules/delivery/delivery.resolvers.ts
@@ -0,0 +1,45 @@
+import { toGlobalId } from 'graphql-relay';
+import { GQLResolvers } from '../../generated/schema';
+import { idResolver } from '../helper/relayResolver';
+import { Delivery } from '../../../db/models/Delivery';
+
+import { model2cursor } from './delivery.loader';
+
+const resolvers: GQLResolvers = {
+  Delivery: {
+    _id: ({ id }) => id,
+    id: idResolver<Delivery>('id', 'Delivery'),
+    deliveryManId: idResolver<Delivery>('delivery_man_id', 'DeliveryMan'),
+    recipientId: idResolver<Delivery>('recipient_id', 'Recipient'),
+    signatureId: ({ signature_id }) =>
+      signature_id ? toGlobalId('Avatar', signature_id) : null,
+    deliveryMan: ({ delivery_man_id }, _args, ctx) =>
+      ctx.dataloaders.DeliveryMan.load(delivery_man_id),
+    recipient: ({ recipient_id }, _args, ctx) =>
+      ctx.dataloaders.Recipient.load(recipient_id),
+    signature: ({ signature_id }, _args, ctx) =>
+      signature_id ? ctx.dataloaders.Avatar.load(signature_id) : null,
+  },
+  DeliveryEdge: {
+    node: ({ node }, _args, ctx) =>
+      node ? ctx.dataloaders.Delivery.load(node.id) : null,
+  },
+  Mutation: {
+    async createDelivery(_parent, args, ctx) {
+      const { deliveryManId, product, recipientId } = args.input;
+      const delivery = await ctx.models.Delivery.create({
+        delivery_man_id: deliveryManId,
+        product,
+        recipient_id: recipientId,
+      });
+      return {
+        edge: {
+          cursor: model2cursor(delivery),
+          node: delivery,
+        },
+      };
+    },
+  },
+};
+
+export default resolvers;
