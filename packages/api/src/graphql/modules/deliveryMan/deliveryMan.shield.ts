368db8a8cefab5cbe23ca513f98cbd0401760984
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2020-03-16 00:42:35 -0300

    feat(delivery-man): add shield info for public access on del man query

---
+++ packages/api/src/graphql/modules/deliveryMan/deliveryMan.shield.ts
@@ -0,0 +1,11 @@
+import { allow } from 'graphql-shield';
+
+export default {
+  DeliveryMan: allow,
+  DeliveryConnection: allow,
+  DeliveryEdge: allow,
+  Delivery: allow,
+  Recipient: allow,
+  Avatar: allow,
+  PageInfo: allow,
+};
