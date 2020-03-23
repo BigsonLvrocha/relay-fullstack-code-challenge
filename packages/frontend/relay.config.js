fc07b16347482f1fa40d875f44864e51f96b2616
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2020-03-23 08:56:58 -0300

    fix(relay): add emailAddress type

--- packages/frontend/relay.config.js
+++ packages/frontend/relay.config.js
@@ -5,4 +5,7 @@ module.exports = {
   schema: './data/schema.graphql',
   language: 'typescript',
   exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__generated__/**'],
+  customScalars: {
+    EmailAddress: 'string',
+  },
 };
