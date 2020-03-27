35a7c2b142c76f42716762ad2215d11237ed7452
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2020-03-26 23:11:58 -0300

    chore(type): add png and svg modules definitions

--- packages/frontend/src/react-app-env.d.ts
+++ packages/frontend/src/react-app-env.d.ts
@@ -2,3 +2,12 @@
 declare module 'babel-plugin-relay/macro' {
   export { graphql } from 'react-relay';
 }
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
