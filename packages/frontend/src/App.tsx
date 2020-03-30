7c1afff4c37129ca20ec30182e49e4ca6d7da64a
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2020-03-29 23:55:11 -0300

    feat(app): add main query

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,71 @@
 import * as React from 'react';
-import { useMeStore } from './store/me';
+import { graphql } from 'babel-plugin-relay/macro';
+import {
+  createRefetchContainer,
+  QueryRenderer,
+  RelayRefetchProp,
+} from 'react-relay';
+
+import { getAuthToken, setAuthToken } from './services/session';
+import { useRelayEnv } from './store/relayEnv';
 import { Router } from './router';
+import { AppQuery } from './__generated__/AppQuery.graphql';
+import { App_me } from './__generated__/App_me.graphql';
 
-function App() {
-  const { state } = useMeStore();
-  return state.checked ? <Router /> : <div>loading...</div>;
-}
+const AppMainQuery = graphql`
+  query AppQuery {
+    ...App_me
+  }
+`;
 
-export default App;
+const MeCtx = React.createContext({
+  me: null as App_me | null,
+  refetchMe: ((() => {}) as unknown) as RelayRefetchProp['refetch'],
+});
+
+const App: React.FunctionComponent<{
+  me: App_me | null;
+  relay: RelayRefetchProp;
+}> = ({ me, relay }) => {
+  return (
+    <MeCtx.Provider value={{ me, refetchMe: relay.refetch }}>
+      <Router />
+    </MeCtx.Provider>
+  );
+};
+
+const AppWithRefetch = createRefetchContainer(
+  App,
+  {
+    me: graphql`
+      fragment App_me on Query {
+        me {
+          id
+          name
+          email
+        }
+      }
+    `,
+  },
+  AppMainQuery,
+);
+
+const AppWithMainQuery: React.FunctionComponent = () => {
+  const tokenInSession = getAuthToken();
+  const env = useRelayEnv();
+  return (
+    <QueryRenderer<AppQuery>
+      query={tokenInSession ? AppMainQuery : undefined}
+      environment={env}
+      variables={{}}
+      render={({ error, props }) => {
+        if (error && error.message === 'Not Authorized') {
+          setAuthToken(null);
+        }
+        return <AppWithRefetch me={props} />;
+      }}
+    />
+  );
+};
+
+export default AppWithMainQuery;
