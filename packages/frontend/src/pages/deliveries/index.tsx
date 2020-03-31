b00df9acbf76fff87c97c4c7b2ee8b329c2e8a5f
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2020-03-30 21:38:41 -0300

    feat(deliveries): implements base for deliveries

--- packages/frontend/src/pages/deliveries/index.tsx
+++ packages/frontend/src/pages/deliveries/index.tsx
@@ -1,3 +1,82 @@
+import { faSearch, faPlus } from '@fortawesome/free-solid-svg-icons';
 import * as React from 'react';
 
-export const Deliveries = () => <div>hello deliveries</div>;
+import {
+  ActionRow,
+  AddButton,
+  AddIcon,
+  AddText,
+  Container,
+  ListTable,
+  SearchBox,
+  SearchIcon,
+  SearchText,
+  Title,
+} from './styles';
+
+export const Deliveries = () => (
+  <Container>
+    <Title>Entregas</Title>
+    <ActionRow>
+      <SearchBox>
+        <SearchIcon icon={faSearch} />
+        <SearchText placeholder="Buscar por encomenda" />
+      </SearchBox>
+      <AddButton>
+        <AddIcon icon={faPlus} />
+        <AddText>CADASTRAR</AddText>
+      </AddButton>
+    </ActionRow>
+    <ListTable>
+      <tr className="header">
+        <th>ID</th>
+        <th>Destinatário</th>
+        <th>Entregador</th>
+        <th>Cidade</th>
+        <th>Estado</th>
+        <th>Status</th>
+        <th>Ações</th>
+      </tr>
+      <tr>
+        <td>#01</td>
+        <td>Bethoven</td>
+        <td>John Doe</td>
+        <td>São Paulo</td>
+        <td>SP</td>
+        <td>Entregue</td>
+        <td>ponts</td>
+      </tr>
+      <tr className="spacing" />
+      <tr>
+        <td>#01</td>
+        <td>Bethoven</td>
+        <td>John Doe</td>
+        <td>São Paulo</td>
+        <td>SP</td>
+        <td>Entregue</td>
+        <td>ponts</td>
+      </tr>
+      <tr className="spacing" />
+      <tr>
+        <td>#01</td>
+        <td>Bethoven</td>
+        <td>John Doe</td>
+        <td>São Paulo</td>
+        <td>SP</td>
+        <td>Entregue</td>
+        <td>ponts</td>
+      </tr>
+      <tr className="spacing" />
+      <tr>
+        <td>#01</td>
+        <td>Bethoven</td>
+        <td>John Doe</td>
+        <td>São Paulo</td>
+        <td>SP</td>
+        <td>Entregue</td>
+        <td>ponts</td>
+      </tr>
+      <tr className="spacing" />
+    </ListTable>
+  </Container>
+);
