9b529d6dfeef4f42c36defeba612f1761356838d
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2021-02-28 12:25:41 -0300

    chore(eslint): format the formmater file

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  ignorePatterns: ["temp.js", "node_modules", "generated"],
+  ignorePatterns: ['temp.js', 'node_modules', 'generated'],
   extends: [
     'plugin:react/recommended',
     'airbnb',
@@ -19,9 +19,9 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
-        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }]
-      }
-    }
+        '@typescript-eslint/no-unused-vars': [2, { args: 'none' }],
+      },
+    },
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -31,44 +31,39 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'prettier',
-    'react-hooks',
-  ],
+  plugins: ['react', '@typescript-eslint', 'prettier', 'react-hooks'],
   rules: {
-    "import/prefer-default-export": "off",
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        "js": "never",
-        "mjs": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
+        js: 'never',
+        mjs: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "camelcase": "off",
-    "no-underscore-dangle": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", "tsx"] }],
-    "react/prop-types": "off",
+    camelcase: 'off',
+    'no-underscore-dangle': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'tsx'] }],
+    'react/prop-types': 'off',
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'import/no-unresolved': [
       'error',
       {
-        'ignore': [ '\.svg' ]
-      }
-    ]
-  }
+        ignore: ['.svg'],
+      },
+    ],
+  },
 };
