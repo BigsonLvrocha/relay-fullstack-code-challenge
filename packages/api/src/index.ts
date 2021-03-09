0890894945a3f875de34f9417524e6abf8eaeb12
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2021-03-08 21:13:34 -0300

    refactor(app): use refactored function

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,25 +1,6 @@
-import { errorHandler } from 'graphql-api-koa';
-import Bodyparser from 'koa-bodyparser';
-import Koa from 'koa';
-import logger from 'koa-logger';
-import cors from '@koa/cors';
+import { createServer } from './server';
 
-import { errorMiddleware } from './middleware/errorMiddleware';
-import { authMiddleware } from './middleware/authMiddleware';
-import { router } from './router';
-
-const app = new Koa();
-
-app.use(cors());
-app.use(Bodyparser());
-app.use(logger());
-app.use(errorHandler());
-app.use(errorMiddleware);
-app.use(authMiddleware);
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-app.listen(8000, () => {
+createServer().app.listen(8000, () => {
   // eslint-disable-next-line no-console
   console.log('server is running');
 });
