7ead35b1b4b08456b23b4fcc63d63d6509165a7f
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2021-07-07 19:14:57 -0300

    chore(jest): fix test match

--- jest.config.ts
+++ jest.config.ts
@@ -154,9 +154,8 @@ const config: Config.InitialOptions = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    '**/__tests__/**/*.[jt]s?(x)',
     '**/?(*.)+(spec|test).[tj]s?(x)',
-    '**/__tests__/**/*.spec.ts?(x)',
+    '**/__tests__/**/*.(spec|test).[tj]s?(x)',
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
