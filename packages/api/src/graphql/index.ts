f35f5de1aa8385c03e18afe15bc4c3e37e565276
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2021-07-07 19:54:56 -0300

    refactor(api): make resolvers injectable

--- packages/api/src/graphql/index.ts
+++ packages/api/src/graphql/index.ts
@@ -1,45 +1,36 @@
 import { join, dirname } from 'path';
-import { fileURLToPath, pathToFileURL } from 'url';
+import { fileURLToPath } from 'url';
 
 import {
   makeExecutableSchema,
-  mergeResolvers,
   mergeTypeDefs,
   loadFilesSync,
-  loadFiles,
 } from 'graphql-tools';
 import { applyMiddleware } from 'graphql-middleware';
 
 import { middlewares } from './middleware';
+import { resolvers as defaultResolvers } from './modules';
+import { GQLResolvers } from './generated/schema';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-async function getResolvers() {
-  const resolversArray = await loadFiles(
-    join(__dirname, 'modules/**/*.resolvers.*s'),
-    {
-      requireMethod: async (path: string) => {
-        return import(pathToFileURL(path).toString());
-      },
-    },
-  );
-  return mergeResolvers(resolversArray);
-}
 
 function getTypes() {
   const typesArray = loadFilesSync(join(__dirname, 'modules/**/*.graphql'));
   return mergeTypeDefs(typesArray);
 }
 
-export async function createSchema() {
+type CreateSchemaOptions = {
+  resolvers?: GQLResolvers;
+};
+
+export function createSchema({
+  resolvers = defaultResolvers,
+}: CreateSchemaOptions = {}) {
   const typeDefs = getTypes();
-  const resolvers = await getResolvers();
   const executableSchema = makeExecutableSchema({
     typeDefs,
     resolvers,
   });
   return applyMiddleware(executableSchema, ...middlewares);
 }
-
-export const schema = await createSchema();
